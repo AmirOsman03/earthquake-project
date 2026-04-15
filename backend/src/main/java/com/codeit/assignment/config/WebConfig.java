498c4d501ba8279d25f8a5cf0bea9cd2cc42f71e
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2026-04-15 14:44:32 +0200

    fix(backend): update CORS configuration

--- backend/src/main/java/com/codeit/assignment/config/WebConfig.java
+++ backend/src/main/java/com/codeit/assignment/config/WebConfig.java
@@ -1,4 +1,18 @@
 package com.codeit.assignment.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*");
+    }
 }
